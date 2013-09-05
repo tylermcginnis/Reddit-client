03f64d8157f292667ede20d5afd07d0d35183fcf
Tyler McGinnis <tylermcginnis33@gmail.com>
2013-09-04 22:23:39 -0700

    all stuff

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,12 @@
-angular.module('myApp.controllers', []);
+angular.module('myApp.controllers', [])
+  .controller('MainController', function($scope, $http){
+    $scope.test = 'fesf';
+    $http({
+      method: 'JSONP',
+      url: 'http://api.reddit.com/r/funny.json?jsonp=JSON_CALLBACK'   
+    }).success(function(data, status){
+      $scope.data = data;
+    });
+  });
+
+
