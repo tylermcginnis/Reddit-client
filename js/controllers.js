0b6e09c4f1582cdcbd47f0b42eb147e42c950890
Tyler McGinnis <tylermcginnis33@gmail.com>
2013-09-05 00:25:45 -0700

    directive and basic layout working. it's late, hope this works in the morning.

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,12 @@
 angular.module('myApp.controllers', [])
   .controller('MainController', function($scope, $http){
-    $scope.test = 'fesf';
     $http({
       method: 'JSONP',
-      url: 'http://api.reddit.com/r/funny.json?jsonp=JSON_CALLBACK'   
+      url: 'http://api.reddit.com/r/funny?jsonp=JSON_CALLBACK'   
     }).success(function(data, status){
-      $scope.data = data.data;
+      $scope.articles = data.data.children;
+      console.log(data.data.children);
     }).error(function(data, status){
       console.log("An error occured");
     });
   });
-
-
--- js/directives.js
+++ js/directives.js
@@ -2,15 +2,20 @@ angular.module('myApp.directives', []).
   directive('articles', function(){
     return {
       restrict: 'A',
-      template: '<h4>HERES THE INFO {{ngModel}}</h4>',
+      replace: true,
+      template: 
+      '<ul>' + 
+        "<li><img ng-src='{{ngModel.data.thumbnail }}'/></li>" +
+        "<li><a href='{{ ngModel.data.url }}'>{{ ngModel.data.title }}</a></li>" +
+      '</ul>',
       require: '^ngModel',
       scope : {
         ngModel: '='
       },
       link: function(scope, element, attr){
-        console.log("SCOPE", scope);
-        console.log("ELEMENT", element);
-        cosole.log("ATTR", attr);
+        // console.log("SCOPE", scope);
+        // console.log("ELEMENT", element);
+        // console.log("ATTR", attr);
       }
     }
   });
